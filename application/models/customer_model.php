a16ee42e9e5ffa6fec055263011697b84a5a2786
Rotha122 <57066736+Rotha122@users.noreply.github.com>
2019-11-06 15:35:03 +0700

    Update customer_model.php

--- application/models/customer_model.php
+++ application/models/customer_model.php
@@ -19,13 +19,13 @@ function display_records()
 	{
 		
 		$query=$this->db->get('customers');
-			return($query->result());
+		return($query->result());
 	}
 
 public function updata($id,$data) {
 
     $this->db->where('id',$id['id']);
-	$this->db->update('customers',$data);
+    $this->db->update('customers',$data);
 	
  	 		
 	
@@ -33,21 +33,22 @@ public function updata($id,$data) {
 }
 
 public function editdata($id) {
- 			$this->db->from('customers');
- 	 		$this->db->where('id',$id);
- 	 		$query = $this->db->get();
- 	 		return $query->row();
+     $this->db->from('customers');
+     $this->db->where('id',$id);
+     $query = $this->db->get();
+     return $query->row();
  	 		//             $query->result();
  }
 
 public function deleterecords($id)
-	{
-		$this->db->where('id',$id);
-		$this->db->delete('customers');
-		//return True;
-	}
+{
+  $this->db->where('id',$id);
+  $this->db->delete('customers');
+	//return True;
+}
   
-    public function searchdata ($keyword) {        
+    public function searchdata ($keyword) 
+    {        
         $this->db->order_by('id', 'DESC');
         $this->db->like("customer_name", $keyword);
         return $this->db->get('customers');
