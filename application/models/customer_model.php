d1dba11eb296c3a95da81b66cf49cf63ea0e1143
Rotha122 <57066736+Rotha122@users.noreply.github.com>
2019-11-06 14:46:36 +0700

    Add files via upload

---
+++ application/models/customer_model.php
@@ -0,0 +1,61 @@
+
+<?php
+
+class customer_model extends CI_Model{
+
+public function _construct(){
+	parent:: _construct();
+
+}
+
+function savedata($data)
+	{
+
+		$this->db->insert('customers', $data);
+
+	}
+
+function display_records()
+	{
+		
+		$query=$this->db->get('customers');
+			return($query->result());
+	}
+
+public function updata($id,$data) {
+
+    $this->db->where('id',$id['id']);
+	$this->db->update('customers',$data);
+	
+ 	 		
+	
+    //return;
+}
+
+public function editdata($id) {
+ 			$this->db->from('customers');
+ 	 		$this->db->where('id',$id);
+ 	 		$query = $this->db->get();
+ 	 		return $query->row();
+ 	 		//             $query->result();
+ }
+
+public function deleterecords($id)
+	{
+		$this->db->where('id',$id);
+		$this->db->delete('customers');
+		//return True;
+	}
+  
+    public function searchdata ($keyword) {        
+        $this->db->order_by('id', 'DESC');
+        $this->db->like("customer_name", $keyword);
+        return $this->db->get('customers');
+    
+}	
+
+}
+
+
+
+
